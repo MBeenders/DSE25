a451b62afe4f83c03c03885d8bb7e47a5fb25a68
Mauro <maubeend@gmail.com>
2023-05-03 11:41:45 +0200

    Added csv compatability to the risk map

--- Management/risk_map/risk_map_generator.py
+++ Management/risk_map/risk_map_generator.py
@@ -1,23 +1,44 @@
 import json
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
 import numpy as np
 
 
+def convert_csv(name: str):
+    data = pd.read_csv(f"management/risk_map/files/{name}.csv")
+
+    converted_data: dict = {}
+    for index, row in data.iterrows():
+        if row["Task ID"] != "":
+            converted_data[row["Task ID"]] = {"Probability": row["Probability"], "Impact": row["Impact"]}
+
+    return converted_data
+
+
 class RiskMapGenerator:
-    def __init__(self, file_start: str, file_after: str, style: str):
+    def __init__(self, file_start: str, style: str, file_after: str = None, csv: bool = False):
         self.figure, self.ax = plt.subplots()
 
         with open(f"styles/{style}.json") as style_file:
             self.style: dict = json.load(style_file)
 
-        with open(f"management/risk_map/files/{file_start}.json") as risk_file:
-            self.risks_start: dict = json.load(risk_file)
+        if csv:
+            self.risks_start = convert_csv(file_start)
 
-        with open(f"management/risk_map/files/{file_after}.json") as risk_file:
-            self.risks_after: dict = json.load(risk_file)
+            if file_after:
+                self.risks_after = convert_csv(file_after)
+        else:
+            with open(f"management/risk_map/files/{file_start}.json") as risk_file:
+                self.risks_start: dict = json.load(risk_file)
+
+            if file_after:
+                with open(f"management/risk_map/files/{file_after}.json") as risk_file:
+                    self.risks_after: dict = json.load(risk_file)
 
         self.start_labels = np.zeros((5, 5, len(self.risks_start)))
-        self.after_labels = np.zeros((5, 5, len(self.risks_after)))
+        if file_after:
+            self.after_labels = np.zeros((5, 5, len(self.risks_after)))
 
     def __create_plot(self, risks, after: bool = False):
         for name, risk in risks.items():
@@ -39,11 +60,16 @@ class RiskMapGenerator:
                 self.start_labels[risk["Impact"] - 1][risk["Probability"] - 1][int(name.split("-")[-1]) - 1] = int(name.split("-")[-1])
 
     def __add_labels(self, risks, labels):
+        wrap_at = 4
         for i in range(len(labels)):
             for j in range(len(labels[i])):
                 non_zeros = labels[i][j][labels[i][j] != 0]
+                row_offset = (len(non_zeros) - 1) * (0.09 / (len(non_zeros % wrap_at) + 1))
                 for k, value in enumerate(non_zeros):
-                    self.ax.text((j + 1.1), (i + 1 + (len(non_zeros) - 1)*0.09 - k*0.18 - 0.05), f"RSK-{int(value)}", fontsize=9)
+                    if k <= wrap_at - 1:
+                        self.ax.text((j + 1.1), (i + 1 + row_offset - k * 0.18 - 0.05), f"RSK-{int(value)}", fontsize=9)
+                    else:
+                        self.ax.text((j + 1.45), (i + 1 + row_offset - (k-wrap_at) * 0.18 - 0.05), f"RSK-{int(value)}", fontsize=9)
 
     def __add_background(self):
         self.ax.fill([0, 0, 12, 9.02],
@@ -64,15 +90,16 @@ class RiskMapGenerator:
         self.__add_labels(self.risks_start, self.start_labels)
         self.__add_background()
 
-        self.ax.set_xlim(0, 5.5)
-        self.ax.set_ylim(0, 5.5)
+        self.ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+        self.ax.set_xlim(0.5, 3.5)
+        self.ax.set_ylim(0.5, 5.5)
 
         plt.xlabel("Probability")
         plt.ylabel("Impact")
         plt.tight_layout()
-        plt.savefig('management/risk_map/output/risk_map_start.pdf')
-        # plt.show()
+        # plt.savefig('management/risk_map/output/risk_map_start.pdf')
+        plt.show()
 
 
-generator = RiskMapGenerator("risks_start", "risks_after", "tudelft_house_style")
+generator = RiskMapGenerator("technical_risks", "tudelft_house_style", csv=True)
 generator.run()
