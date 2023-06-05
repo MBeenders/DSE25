8043b4ca0ae913cf134df519999ed9f9083718e6
Mauro <maubeend@gmail.com>
2023-06-05 16:09:12 +0200

    Restructure of class, added file manager, updated some files

--- engineering/main.py
+++ engineering/main.py
@@ -1,5 +1,6 @@
 from sizing.rocket import Rocket
-import sizing.engine
+from sizing.engine import run as run_engine_sizing
+from sizing.recovery import run as run_recovery_sizing
 from file_manager import initialize_rocket
 
 
@@ -9,6 +10,15 @@ class Runner:
         :param file_name: Name of the rocket initialization file
         """
         self.rocket: Rocket = initialize_rocket(file_name)
+
+    def run_sizing(self, selection: list[str]):
+        """
+        :param selection:
+        """
+        if "engine" in selection:
+            run_engine_sizing(self.rocket)
+        if "recovery" in selection:
+            run_recovery_sizing(self.rocket)
 
 
 if __name__ == "__main__":
---
+++ engineering/sizing/recovery.py
@@ -0,0 +1,27 @@
+from engineering.sizing.rocket import Rocket
+
+
+def do_stuff(rocket: Rocket):
+    """
+    :param rocket: Rocket class
+    :return: None
+    """
+    rocket.mass = 5
+
+
+def run(rocket: Rocket):
+    """
+    :param rocket: Original Rocket class
+    :return: Updated Rocket class
+    """
+
+    print(f"Mass before functions: {rocket.mass}")
+    do_stuff(rocket)
+    print(f"Mass after functions: {rocket.mass}")
+
+    pass
+
+
+if __name__ == "__main__":
+    test_rocket = Rocket()
+    run(test_rocket)
