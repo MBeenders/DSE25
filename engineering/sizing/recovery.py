91cd87b7af4c311193de7d825c4b2be3b29ff07c
Mauro <maubeend@gmail.com>
2023-06-07 12:51:41 +0200

    Broken code

--- engineering/sizing/recovery.py
+++ engineering/sizing/recovery.py
@@ -1,12 +1,64 @@
 from engineering.sizing.rocket import Rocket
 
+#Constant values:
+g = 9.80665
+rho = 1.225
 
-def do_stuff(rocket: Rocket):
+
+
+def Parachutes(rocket: Rocket):
     """
     :param rocket: Rocket class
     :return: None
     """
-    pass
+    dry_mass1 = rocket.stage1.dry_mass
+    dry_mass2 = rocket.stage2.dry_mass
+    V_1 = rocket.stage1.recovery.descent_rate
+    V_2 = rocket.stage2.recovery.descent_rate
+    c_D_main1 = rocket.stage1.recovery.main_parachute.c_D
+    c_D_drogue2 = rocket.stage2.recovery.drogue.c_D
+    c_D_main2 = rocket.stage2.recovery.main_parachute.c_D
+    material_density = rocket.stage1.recovery.material_density #Assume material density is the same for both stages
+    material_cost = rocket.stage1.recovery.material_cost
+
+    # AREA OF PARACHUTES:
+
+    # 1st stage main parachute:
+    S_main1 = dry_mass1 * g / (0.5 * rho * V_1 ** 2 * c_D_main1)
+
+    # 2nd stage drogue parachute:
+    S_drogue2 = dry_mass2 * g / (0.5 * rho * V_2 ** 2 * c_D_drogue2)
+
+    # 2nd stage main parachute:
+    S_main2 = dry_mass2 * g / (0.5 * rho * V_2 ** 2 * c_D_main2)
+
+    # MASS OF PARACHUTES:
+
+    #1st stage main parachute:
+    m_main1 = S_main1 * material_density
+
+    # 2nd stage drogue parachute:
+    m_drogue2 = S_drogue2 * material_density
+
+    # 2nd stage main parachute:
+    m_main2 = S_main2 * material_density
+
+    m_parachutes = [m_main1, m_drogue2, m_main2]
+
+    # COST OF PARACHUTES:
+
+    # 1st stage main parachute:
+    cost_main1 = S_main1 * material_cost
+
+    # 2nd stage drogue parachute:
+    cost_drogue2 = S_drogue2 * material_cost
+
+    # 2nd stage main parachute:
+    cost_main2 = S_main2 * material_cost
+
+    cost_parachutes = [cost_main1, cost_drogue2, cost_main2]
+
+    return m_parachutes, cost_parachutes
 
 
 def run(rocket: Rocket) -> Rocket:
@@ -15,7 +67,7 @@ def run(rocket: Rocket) -> Rocket:
     :return: Updated Rocket class
     """
 
-    do_stuff(rocket)
+    Parachutes(rocket)
 
     return rocket
 
