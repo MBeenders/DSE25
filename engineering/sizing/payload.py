cf0d0f3856070b7336622e39c08e7072cdcc67bd
1ckendall <86923829+1ckendall@users.noreply.github.com>
2023-06-08 08:53:22 +0200

    Create payload.py

---
+++ engineering/sizing/payload.py
@@ -0,0 +1,8 @@
+from engineering.sizing.rocket import Rocket
+
+class Payload():
+    def __init__(self):
+        self.mass = 12
+        self.length = None
+        self.diameter = None
+        self.power_draw = None
