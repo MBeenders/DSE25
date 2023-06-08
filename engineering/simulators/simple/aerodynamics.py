6b52908a0e5e383e82fe8fff18767514659f3a8d
syorucu <syorucu@yahoo.com>
2023-06-08 12:28:04 +0200

    Update aerodynamics.py

--- engineering/simulators/simple/aerodynamics.py
+++ engineering/simulators/simple/aerodynamics.py
@@ -100,37 +100,60 @@ def drag(velocity: np.ndarray, height: float) -> np.ndarray:
             C_fr_c = C_fr_c
 
     # The body wetted area is corrected for its cylindrical geometry, and the fins for their finite thickness.
-<<<<<<< Updated upstream:engineering/simulators/simple/aerodynamics.py
+    D_u = 0.15  # Upper stage diameter [m]
+    D_l = 0.2  # Lower stage diameter [m]
     f_B = 15  # Fineness ratio of the rocket ===== MAKE VARIABLE FORM
     S_w_b = 4000  # Wet surface area of body [m^2] ===== MAKE VARIABLE FORM
     S_w_fin = 20  # Wet surface area of body [m^2] ===== MAKE VARIABLE FORM
     t_fin = 0.005  # Thickness of fins [m] ===== MAKE VARIABLE FORM
     mac_fin = 0.5  # Mean aerodynamic chord length of fins [m] ===== MAKE VARIABLE FORM
-    C_D_f = C_f_c * (((1+(1/(2*f_B)))*S_w_b) + ((1+(2*t_fin/mac_fin))*S_w_fin))/(S_ref)
-    D_f = C_D_f * (0.5 * S_w * density(height) * velocity ** 2)  # Total frictional drag force of vehicle [N]
+    C_D_fr = C_fr_c * (((1+(1/(2*f_B)))*S_w_b) + ((1+(2*t_fin/mac_fin))*S_w_fin))/(S_ref)
+    D_fr = C_D_fr * (0.5 * S_w * density(height) * velocity ** 2)  # Total frictional drag force of vehicle [N]
 
-    # BODY PRESSURE DRAG
+    # BODY PRESSURE DRAG (apply for both stages)
     phi = 22 * np.pi/180  # Nose cone joint angle [rad] ===== MAKE VARIABLE FORM
-    C_D_nc = 0.8 * np.sin(phi)**2  # Approximate nose pressure drag coefficient at M=0
-=======
     f_B = 15 # Fineness ratio of the rocket ===== MAKE VARIABLE FORM
     S_w_b = 4000 # Wet surface area of body [m^2] ===== MAKE VARIABLE FORM
     S_w_fin = 20 # Wet surface area of body [m^2] ===== MAKE VARIABLE FORM
     t_fin = 0.005 # Thickness of fins [m] ===== MAKE VARIABLE FORM
     mac_fin = 0.5 # Mean aerodynamic chord length of fins [m] ===== MAKE VARIABLE FORM
-    C_D_fr = C_fr_c * (((1+(1/(2*f_B)))*S_w_b) + ((1+(2*t_fin/mac_fin))*S_w_fin))/(S_ref)
-    D_fr = C_D_fr * S_w * q # Total frictional drag force of vehicle [N]
+    C_D_f = C_fr_c * (((1+(1/(2*f_B)))*S_w_b) + ((1+(2*t_fin/mac_fin))*S_w_fin))/(S_ref)
+    D_f = C_D_f * S_w * q # Total frictional drag force of vehicle [N]
 
-    # BODY PRESSURE DRAG
+    # NOSE CONE PRESSURE DRAG
+    # NOSECONE - 5:1 Haack (optimised for length and diameter, where c = 0; also optimised for wave and skin friction drag)
+
+    # 5:1 L-D Haack minimum drag noses
+    c = 0  # Optimisation coefficient        [-]
+    X = 0.024  # Axial distance from the nose [m]
+    L = 5 * D_u  # Model length                  [m]
+    R = 0.075  # Model base radius            [m]
+
+    phi = np.arccos(1 - 2 * (X / L))  # [rad]
+    r = (R / np.sqrt(np.pi)) * np.sqrt(phi - 0.5 * np.sin(2 * phi) + c * np.sin(phi) ** 3)  # Model local radius [m]
+
     f_N = 5 # Fineness ratio of nose
     phi = 22 * np.pi/180 # Nose cone joint angle [rad] ===== MAKE VARIABLE FORM
     C_D_nc = 0.8 * np.sin(phi)**2 # Approximate nose pressure drag coefficient at M=0, phi<30 [deg]
+    b = 0.3 # ===== I HAVE NO IDEA WHAT THIS EMPIRICAL CONSTANT IS!!!
     C_D_nc_s = C_nc_0 * (4**b) ** (np.log(f_N + 1)/np.log(4)) # Includes supersonic factor
     C_D_nc = C_D_nc_s + C_D_nc # Includes compressibility correction for sub and supersonic speeds
-    S_nc = 89
-    D_nc = C_D_nc * q * S_nc
+    S_nc = np.pi * D_u # Surface area of nose cone [m^2] ===== NO IDEA HOW TO FIND IT!!!
+    D_nc = C_D_nc * q * S_nc # Nose cone pressure drag [N]
+
+    # SHOULDER PRESSURE DRAG (separation stage)
+    if M <=1:
+        C_D_sh = C_D_nc
+    else:
+        C_D_sh = 0
+
+    L_t = 0.2 # Separation stage length [m]
+    theta = np.arcsin((D_l-D_u)/(2*L_t))
+    S_sh = np.pi * D_l * L_t*np.cos(theta) # Shoulder wet surface area [m^2]
+    D_sh = C_D_sh * q * S_sh # Shoulder pressure drag [N]
 
     # FIN PRESSURE DRAG (Assume that flow is perpendicular to the leading edge)
+    # Assume for fins with tapering trailing edges, the base drag is zero.
     if M <= 0.9:
         C_D_LE = -1 + (1-M**2)**(-0.417)
     elif 0.9 < M <= 1:
@@ -139,10 +162,10 @@ def drag(velocity: np.ndarray, height: float) -> np.ndarray:
         C_D_LE = 1.214 - 0.502/(M**2) + 0.1095/(M**4)
 
     G_LE = 45 * np.pi/180 # Leading edge angle of slanted fin [rad]
-    C_D_LE = C_D_LE
-    C_D_LE = C_D_LE * np.cos(G_LE)**2
-    S_w_fin = t_fin
-    D_fin = C_D_LE * q * S_fin
+    C_D_LE = C_D_LE * np.cos(G_LE)**2 # Slanted fin cross-flow principle [-]
+    b = 0.225 # Fin span [m]
+    S_fin = t_fin * b / np.cos(G_LE) # Surface area of fins that the flow sees [m^2]
+    D_fin = C_D_LE * q * S_fin # Fin pressure drag [N]
 
     # BASE DRAG
     if M <= 1:
@@ -150,16 +173,12 @@ def drag(velocity: np.ndarray, height: float) -> np.ndarray:
     if M > 1:
         C_D_base = 0.25/M
 
-    D_l = 0.2
-    t = 0.003
-    S_base = np.pi * D_l * t # Surface area of base (Diameter of lower stage * thickness * PI)
-    D_base = C_D_base * q * S_base
->>>>>>> Stashed changes:engineering/simulators/aerodynamics.py
+    t = 0.003 # Skin thickness [m]
+    S_base = np.pi * D_l * t # Surface area of base (Diameter of lower stage * thickness * PI) [m^2]
+    D_base = C_D_base * q * S_base # Base drag [N]
 
-    drag_coefficient: float = 0.5  # -
-    radius: float = 0.3  # m
-    frontal_area: float = np.pi * radius**2  # m^2
-    force_drag = drag_coefficient * frontal_area * 0.5 * density(height) * velocity**2
+    # TOTAL DRAG FORCE [N]
+    force_drag = D_base + 4*D_fin + D_sh + D_nc + D_f + D_fr
 
     return force_drag
 
