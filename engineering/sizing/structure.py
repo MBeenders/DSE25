c2b398ae72f494b10e994bd751b9722b60e5bd5c
Mauro <maubeend@gmail.com>
2023-06-08 13:20:47 +0200

    Fixed structure import

--- engineering/sizing/structure.py
+++ engineering/sizing/structure.py
@@ -1,14 +1,7 @@
-import sys
-import os
-
-path = os.path.split(sys.argv[0])[0]
-sys.path.append(path)
-
-from sizing.rocket import Rocket
 import numpy as np
 
 
-def do_stuff(rocket: Rocket):
+def do_stuff(rocket):
     """
     :param rocket: Rocket class
     :return: None
@@ -16,7 +9,7 @@ def do_stuff(rocket: Rocket):
     pass
 
 
-def run(rocket: Rocket) -> Rocket:
+def run(rocket):
     """
     :param rocket: Original Rocket class
     :return: Updated Rocket class
@@ -28,5 +21,4 @@ def run(rocket: Rocket) -> Rocket:
 
 
 if __name__ == "__main__":
-    test_rocket = Rocket()
-    run(test_rocket)
+    pass
