a094545cdde38e53b1d7c12e96c4c9f62afa8703
1ckendall <86923829+1ckendall@users.noreply.github.com>
2023-06-13 11:47:35 +0200

    Create yoyo_despin.py

---
+++ engineering/sizing/yoyo_despin.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# NASA TN D-708
+
+def size_yoyo_mass(rocket, wraps = 2)
+    vehicle_inertia = rocket.stage2.mmoi # not yet implemented
+    l = np.pi()*rocket.stage2.diameter
+    stringweight = 2 * 980 * l * np.pi() * 1E-3**2 #density of dyneema is 980kg/m^3, multiply this by line volume
+    m = vehicle_inertia/((l+diameter/2)**2)
+    return m-1/3*stringweight
+
+def yoyo_mmoi_contribution(rocket, mass)
+    return mass*(rocket.diameter/2)**2
